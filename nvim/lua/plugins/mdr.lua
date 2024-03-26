3f3afdbfc968a5c615655d35060ad599c568843d
SmokingYoda <kurtakkohenri@protonmail.com>
2024-03-26 20:22:07 +0200

    fzf and mappings

--- nvim/lua/plugins/mdr.lua
+++
@@ -1,5 +0,0 @@
-return function()
-	for buf in pairs(vim.api.nvim_list_wins()) do
-		print(vim.api.nvim_win_get_buf(buf))
-	end
-end
--- nvim/lua/test.lua
+++
@@ -1,3 +0,0 @@
-return function()
-	print(vim.fn.bufname())
-end
